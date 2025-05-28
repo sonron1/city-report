0c67a42f34e913ae712345b11b4bd432753b5656
sonron1 <lolathread@gmail.com>
2025-05-28 20:10:45 +0100

    Update webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
+    .addEntry('signalement-map', './assets/js/signalement-map.js')
+    .addEntry('carte-index', './assets/js/carte-index.js') // Ajoutez cette ligne
     .addStyleEntry('styles', './assets/styles/app.scss')
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
