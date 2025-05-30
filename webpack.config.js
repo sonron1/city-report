877a33b284dd06eb8c78fec61e5da8c6cb18e588
sonron1 <lolathread@gmail.com>
2025-05-30 09:04:05 +0100

    Commit Update Coding Interface and Backend Communication with Postgrsql, Auth service and map leaflet integration.

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,10 @@ Encore
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
     .addEntry('signalement-map', './assets/js/signalement-map.js')
-    .addEntry('carte-index', './assets/js/carte-index.js') // Ajoutez cette ligne
+    .addEntry('carte-index', './assets/js/carte-index.js')
+    .addEntry('admin', './assets/js/admin.js') // Script pour l'administration
     .addStyleEntry('styles', './assets/styles/app.scss')
+    .addStyleEntry('admin-styles', './assets/styles/admin.scss') // Styles pour l'administration
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
@@ -14,6 +16,17 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
     .enableSassLoader()
+    // Copier les images de Leaflet
+    .copyFiles({
+        from: './node_modules/leaflet/dist/images',
+        to: 'images/[path][name].[ext]'
+    })
+    // Copier les images pour l'administration
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+        pattern: /\.(png|jpg|jpeg|svg|gif)$/
+    })
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = '3.23';
